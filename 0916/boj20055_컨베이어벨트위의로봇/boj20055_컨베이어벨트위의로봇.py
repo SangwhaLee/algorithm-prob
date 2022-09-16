b897d4a9ef8e6616335e8b7392b5f40abca1ec9e
SangwhaLee <sanghwa1103@gmail.com>
2022-09-16 17:18:30 +0900

    집에가서 마저하자

--- 0916/boj20055_컨베이어벨트위의로봇/boj20055_컨베이어벨트위의로봇.py
+++ 0916/boj20055_컨베이어벨트위의로봇/boj20055_컨베이어벨트위의로봇.py
@@ -1,11 +1,10 @@
 from collections import deque
 
-def robot_conveyor(belt):
-
- L
-
-
 N, k = map(int, input().split())
 
 belt = list(map(int,input().split()))
 
+
+while True:
+    # 1. 회전: belt의 각 구성요소를 다음 인덱스로 이동 마지막 인덱스에 있는 값은 첫 인덱스로 이동
+    # 2. 가장 뒤쪽에 있는 로봇의 다음 인덱스가 비어있다면 이동, 로봇이 이
