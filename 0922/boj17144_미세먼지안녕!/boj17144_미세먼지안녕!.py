3a0373a707d63d9b7889b1da635637e82e12c25b
SangwhaLee <sanghwa1103@gmail.com>
2022-09-23 01:57:36 +0900

    0922 done

--- 0922/boj17144_미세먼지안녕!/boj17144_미세먼지안녕!.py
+++ 0922/boj17144_미세먼지안녕!/boj17144_미세먼지안녕!.py
@@ -19,7 +19,7 @@ def dust_diffusion():
                 diffused_room[i][j] -= (room[i][j]//5)*cnt
     room = diffused_room
 
-
+# 공기청정기의 각 영역의 가장 뒤쪽에서부터 시작해 바람의 흐름과 반대방향으로 각 미세먼지가 이동한다
 def fresher_on():
     global room
     # 공기청정기 기준 위쪽에 있는 영역부터 해결
@@ -38,7 +38,7 @@ def fresher_on():
             now[0] += 1
         elif now[0] == st[0] and now[1] == C-1:
             room[now[0]][now[1]] = room[now[0]][now[1]-1]
-            now[0] -= 1
+            now[1] -= 1
         elif now[1] == 0:
             room[now[0]][now[1]] = room[now[0]-1][now[1]]
             now[0] -= 1
@@ -48,11 +48,12 @@ def fresher_on():
         elif now[1] == C-1:
             room[now[0]][now[1]] = room[now[0]+1][now[1]]
             now[0] += 1
-        else:
+        elif now[0] == st[0]:
             room[now[0]][now[1]] = room[now[0]][now[1] -1]
             now[1] -= 1
         step -= 1
 
+    # 공기 청정기 아래쪽의 영역을 해결
     st = (fresher[1], 0)
     step = 2*C + 2*(R-st[0]-1) - 3
     now = [st[0]+1, st[1]]
@@ -78,7 +79,7 @@ def fresher_on():
         elif now[1] == C-1:
             room[now[0]][now[1]] = room[now[0]-1][now[1]]
             now[0] -= 1
-        else:
+        elif now[0] == st[0]:
             room[now[0]][now[1]] = room[now[0]][now[1]-1]
             now[1] -= 1
         step -= 1
@@ -107,5 +108,5 @@ for i in range(R):
         if room[i][j] != -1:
             total += room[i][j]
 
-print(room)
+# print(room)
 print(total)
